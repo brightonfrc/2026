2ed983ff6e74bc1c18a01cc18e39c917cfb13ed4
Natalie <110317525+nataly8420@users.noreply.github.com>
2026-02-25 16:11:58 +0000

    Add files via upload
    re-re-uploaded modified src/main

---
+++ src/main/java/frc/robot/commands/CoralStationAlign.java
@@ -0,0 +1,130 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.CoralStationAlignConstants;
+import frc.robot.Constants.FieldOrientedDriveConstants;
+import frc.robot.Constants.TestingConstants;
+import frc.robot.subsystems.DriveSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class CoralStationAlign extends Command {
+  private DriveSubsystem driveSubsystem;
+  private PIDController bearingPIDController;
+  private CommandXboxController controller;
+  private boolean active;
+  /** 
+   * Creates a new CoralStationAlign, which locks the robot's bearing towards the left coral station
+   * Coral station require is decided by the general direction of left joystick
+   * cancel by pressing right bumper
+   */
+  // public CoralStationAlign(DriveSubsystem driveSubsystem, CommandXboxController controller, AprilTagPoseEstimator estimator) {
+  //   this.driveSubsystem=driveSubsystem;
+  //   this.controller=controller;
+  //   this.estimator=estimator;
+  //   // Use addRequirements() here to declare subsystem dependencies.
+  //   addRequirements(driveSubsystem, estimator);
+  // }
+  public CoralStationAlign(DriveSubsystem driveSubsystem, CommandXboxController controller) {
+    this.driveSubsystem=driveSubsystem;
+    this.controller=controller;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    //remember that gyro is flipped
+      bearingPIDController=new PIDController(
+        CoralStationAlignConstants.kRotP,
+        CoralStationAlignConstants.kRotI,
+        CoralStationAlignConstants.kRotD
+      );
+      //left coral station bearing is at 126 to 0, where 0 is forwards for the robot. 
+      
+      double currentBearing=driveSubsystem.getGyroAngle();
+      if (currentBearing<0){
+        currentBearing+=360;
+      }
+      // SmartDashboard.putNumber("robotBearing", currentBearing);
+      if (currentBearing<180){
+        // SmartDashboard.putBoolean("SetRightCS", false);
+        bearingPIDController.setSetpoint(CoralStationAlignConstants.leftCoralStationRot*Math.PI/180);
+      }
+      else{
+        // SmartDashboard.putBoolean("SetRightCS", true);
+        bearingPIDController.setSetpoint(CoralStationAlignConstants.rightCoralStationRot*Math.PI/180);
+      }
+      bearingPIDController.setTolerance(FieldOrientedDriveConstants.bearingTolerance);
+      bearingPIDController.enableContinuousInput(0, 2*Math.PI);
+      // xPIDController=new PIDController(
+      //   AprilTagAlignmentConstants.kMoveP, 
+      //   AprilTagAlignmentConstants.kMoveI, 
+      //   AprilTagAlignmentConstants.kMoveD
+      // );
+      // xPIDController.setSetpoint(CoralStationAlignConstants.xDisplacement);
+      // xPIDController.setSetpoint(CoralStationAlignConstants.xTolerance);
+      active=true;
+}
+  
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    SmartDashboard.putNumber("robotBearing", driveSubsystem.getGyroAngle());
+    SmartDashboard.putBoolean("Coral Station Align Active", true);
+    //check to end command
+    if (controller.rightBumper().getAsBoolean()){
+      //end command the moment rightBumper is pressed
+      active=false;
+    }
+    if (Math.hypot(controller.getRightY(), controller.getRightX())> 0.9) {
+      if (controller.getRightX()>0){
+        //right stick facing right, which swaps coral station from left to Right. Bearing 240
+        bearingPIDController.reset();
+        bearingPIDController.setSetpoint(CoralStationAlignConstants.rightCoralStationRot*Math.PI/180);
+      }
+      else{
+        bearingPIDController.reset();
+        bearingPIDController.setSetpoint(CoralStationAlignConstants.leftCoralStationRot*Math.PI/180);
+      }
+    }
+    double xSpeed=0;
+    double bearing = Math.toRadians(driveSubsystem.getGyroAngle());
+    double joystickMoveBearing = Math.atan2(controller.getLeftX(), -controller.getLeftY());
+    double joystickMoveMagnitude = Math.hypot(controller.getLeftX(), controller.getLeftY());
+    // Optional<Transform3d> pose = estimator.getRobotToSeenTag();
+    // if (pose.isEmpty()){
+    //   SmartDashboard.putBoolean("April Tag in view", false);
+    //   xSpeed= joystickMoveMagnitude * Math.cos(joystickMoveBearing) * TestingConstants.maximumSpeedReduced;
+    // }
+    // else{
+    //   SmartDashboard.putBoolean("April Tag in view", true);
+    //   SmartDashboard.putNumber("XDisplacement", pose.get().getX());
+    //   //reverse xSpeed because drivetrain must drive at positive sped to reduce xdistance
+    //   xSpeed= -xPIDController.calculate(pose.get().getX());
+    // }
+    xSpeed= joystickMoveMagnitude * Math.cos(joystickMoveBearing) * TestingConstants.maximumSpeedReduced;
+    double ySpeed= joystickMoveMagnitude * Math.sin(joystickMoveBearing) * TestingConstants.maximumSpeedReduced;
+    driveSubsystem.drive(xSpeed, -ySpeed, bearingPIDController.calculate(bearing), false);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    SmartDashboard.putBoolean("Coral Station Align Active", false);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return !active;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,345 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.hal.FRCNetComm.tInstances;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.Constants.AutonPathPlannerConstants;
+import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.studica.frc.AHRS;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.RobotBase;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.config.PIDConstants;
+
+
+public class DriveSubsystem extends SubsystemBase {
+  // Create MAXSwerveModules
+  private final MAXSwerveModule m_frontLeft = new MAXSwerveModule(
+      DriveConstants.kFrontLeftDrivingCanId,
+      DriveConstants.kFrontLeftTurningCanId,
+      DriveConstants.kFrontLeftChassisAngularOffset);
+
+  private final MAXSwerveModule m_frontRight = new MAXSwerveModule(
+      DriveConstants.kFrontRightDrivingCanId,
+      DriveConstants.kFrontRightTurningCanId,
+      DriveConstants.kFrontRightChassisAngularOffset);
+
+  private final MAXSwerveModule m_rearLeft = new MAXSwerveModule(
+      DriveConstants.kRearLeftDrivingCanId,
+      DriveConstants.kRearLeftTurningCanId,
+      DriveConstants.kBackLeftChassisAngularOffset);
+
+  private final MAXSwerveModule m_rearRight = new MAXSwerveModule(
+      DriveConstants.kRearRightDrivingCanId,
+      DriveConstants.kRearRightTurningCanId,
+      DriveConstants.kBackRightChassisAngularOffset);
+
+  // The gyro sensor
+  private final AHRS m_gyro = new AHRS(AHRS.NavXComType.kMXP_SPI, AHRS.NavXUpdateRate.k50Hz);
+  public double getTilt(){
+    if(Math.abs(m_gyro.getPitch())>Math.abs(m_gyro.getRoll()))
+    {
+      return Math.abs(m_gyro.getPitch());
+    } else{
+      return Math.abs(m_gyro.getRoll());
+    }
+  }
+
+  // Odometry class for tracking robot pose
+  SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
+      DriveConstants.kDriveKinematics,
+      Rotation2d.fromDegrees(m_gyro.getAngle()%360),
+      new SwerveModulePosition[] {
+          m_frontLeft.getPosition(),
+          m_frontRight.getPosition(),
+          m_rearLeft.getPosition(),
+          m_rearRight.getPosition()
+      });
+
+  // Simple pose integration for simulation (no drivetrain physics)
+  private Pose2d m_simPose = new Pose2d();
+  private ChassisSpeeds m_lastCommandedSpeeds = new ChassisSpeeds();
+  private double m_lastSimTimeSec = Timer.getFPGATimestamp();
+  
+  //for Choreo
+  // private final PIDController xController = new PIDController(ChoreoConstants.translationkP, ChoreoConstants.translationkI, ChoreoConstants.translationkD);
+  // private final PIDController yController = new PIDController(ChoreoConstants.translationkP, ChoreoConstants.translationkI, ChoreoConstants.translationkD);
+  // private final PIDController headingController = new PIDController(ChoreoConstants.rotationkP, ChoreoConstants.rotationkI, ChoreoConstants.rotationkD);
+
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {
+    if (RobotBase.isSimulation()) {
+      // Skip PathPlanner GUI config loading in sim; it requires deploy files.
+      HAL.report(tResourceType.kResourceType_RobotDrive, tInstances.kRobotDriveSwerve_MaxSwerve);
+      return;
+    }
+    RobotConfig config;
+    try{
+      config = RobotConfig.fromGUISettings();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return;
+    }
+    AutoBuilder.configure(
+      this::getPose,
+      this::resetOdometry,
+      this::getChassisSpeeds,
+      (speeds, feedforwards) -> chassisDrive(speeds),
+      new PPHolonomicDriveController(
+        new PIDConstants(AutonPathPlannerConstants.translationkP, AutonPathPlannerConstants.translationkI, AutonPathPlannerConstants.translationkD),//new PIDConstants(0.4, 0.0, 0.4),
+        new PIDConstants(AutonPathPlannerConstants.rotationkP, AutonPathPlannerConstants.rotationkI, AutonPathPlannerConstants.rotationkD)//new PIDConstants(0.4, 0.0, 0.0)
+      ),
+      config,
+      () -> {
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+          return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+      },
+      this
+      );
+    // Usage reporting for MAXSwerve template
+    HAL.report(tResourceType.kResourceType_RobotDrive, tInstances.kRobotDriveSwerve_MaxSwerve);
+  }
+
+  //for Choreo
+  // public void followTrajectory(SwerveSample sample) {
+  //       // Get the current pose of the robot
+  //       Pose2d pose = getPose();
+
+  //       // Generate the next speeds for the robot
+  //       ChassisSpeeds speeds = new ChassisSpeeds(
+  //           sample.vx + xController.calculate(pose.getX(), sample.x),
+  //           sample.vy + yController.calculate(pose.getY(), sample.y),
+  //           sample.omega + headingController.calculate(pose.getRotation().getRadians(), sample.heading)
+  //       );
+
+  //       // Apply the generated speeds
+  //       drive(speeds.vxMetersPerSecond/DriveConstants.kMaxSpeedMetersPerSecond,speeds.vyMetersPerSecond/DriveConstants.kMaxSpeedMetersPerSecond,0,false);
+  //       // drive(speeds.vxMetersPerSecond/DriveConstants.kMaxSpeedMetersPerSecond,speeds.vyMetersPerSecond/DriveConstants.kMaxSpeedMetersPerSecond,speeds.omegaRadiansPerSecond/DriveConstants.kMaxAngularSpeed,false);
+  //   }
+
+  @Override
+  public void periodic() {
+    // Update the odometry in the periodic block
+    m_odometry.update(
+        Rotation2d.fromDegrees(m_gyro.getAngle()%360),
+        new SwerveModulePosition[] {
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_rearLeft.getPosition(),
+            m_rearRight.getPosition()
+        });
+  }
+
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    Pose2d pose = RobotBase.isSimulation() ? m_simPose : m_odometry.getPoseMeters();
+    // double angle =m_gyro.getAngle()%360;
+    // if (angle<0){
+    //   angle+=360;
+    // }
+    // pose= new Pose2d(pose.getX(), pose.getY(), Rotation2d.fromDegrees(angle));
+    SmartDashboard.putNumber("Pose/x", pose.getX());
+    SmartDashboard.putNumber("Pose/y", pose.getY());
+    SmartDashboard.putNumber("Pose/rot", pose.getRotation().getDegrees());
+    return pose;
+  }
+  /**Returns currently-estimated pose without current bearing */
+  public Pose2d getPoseChoreo(){
+    Pose2d pose=getPose();    
+    //for some reason pose is reversed
+    return new Pose2d(-pose.getX(), -pose.getY(), Rotation2d.fromDegrees(0));
+  }
+
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    if (RobotBase.isSimulation()) {
+      m_simPose = pose;
+      m_lastSimTimeSec = Timer.getFPGATimestamp();
+      return;
+    }
+    m_odometry.resetPosition(
+        Rotation2d.fromDegrees(m_gyro.getAngle()%360),
+        new SwerveModulePosition[] {
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_rearLeft.getPosition(),
+            m_rearRight.getPosition()
+        },
+        pose);
+  }
+
+  /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed        Speed of the robot in the x direction (forward).
+   * @param ySpeed        Speed of the robot in the y direction (sideways).
+   * @param rot           Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the
+   *                      field.
+   */
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+    // Convert the commanded speeds into the correct units for the drivetrain
+    double xSpeedDelivered = xSpeed * DriveConstants.kMaxSpeedMetersPerSecond;
+    double ySpeedDelivered = ySpeed * DriveConstants.kMaxSpeedMetersPerSecond;
+    double rotDelivered = rot * DriveConstants.kMaxAngularSpeed;
+
+    var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
+        fieldRelative
+            ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered,
+                Rotation2d.fromDegrees(m_gyro.getAngle()%360))
+            : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered));
+    // Store the robot-relative commanded speeds for sim pose integration
+    m_lastCommandedSpeeds =
+        fieldRelative
+            ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                xSpeedDelivered, ySpeedDelivered, rotDelivered,
+                Rotation2d.fromDegrees(m_gyro.getAngle()%360))
+            : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered);
+    SwerveDriveKinematics.desaturateWheelSpeeds(
+        swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
+    m_frontLeft.setDesiredState(swerveModuleStates[0]);
+    m_frontRight.setDesiredState(swerveModuleStates[1]);
+    m_rearLeft.setDesiredState(swerveModuleStates[2]);
+    m_rearRight.setDesiredState(swerveModuleStates[3]);
+
+    //SmartDashboard.put
+  }
+
+  /**
+   * Sets the wheels into an X formation to prevent movement.
+   */
+  public void setX() {
+    m_frontLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
+    m_frontRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
+    m_rearLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
+    m_rearRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
+  }
+
+  /**
+   * Sets the swerve ModuleStates.
+   *
+   * @param desiredStates The desired SwerveModule states.
+   */
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(
+        desiredStates, DriveConstants.kMaxSpeedMetersPerSecond);
+    m_frontLeft.setDesiredState(desiredStates[0]);
+    m_frontRight.setDesiredState(desiredStates[1]);
+    m_rearLeft.setDesiredState(desiredStates[2]);
+    m_rearRight.setDesiredState(desiredStates[3]);
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    if (!RobotBase.isSimulation()) {
+      return;
+    }
+    double now = Timer.getFPGATimestamp();
+    double dt = now - m_lastSimTimeSec;
+    if (dt <= 0) {
+      return;
+    }
+    m_lastSimTimeSec = now;
+
+    // Integrate robot-relative chassis speeds into field pose
+    Twist2d twist = new Twist2d(
+        m_lastCommandedSpeeds.vxMetersPerSecond * dt,
+        m_lastCommandedSpeeds.vyMetersPerSecond * dt,
+        m_lastCommandedSpeeds.omegaRadiansPerSecond * dt);
+    m_simPose = m_simPose.exp(twist);
+  }
+
+  /** Resets the drive encoders to currently read a position of 0. */
+  public void resetEncoders() {
+    m_frontLeft.resetEncoders();
+    m_rearLeft.resetEncoders();
+    m_frontRight.resetEncoders();
+    m_rearRight.resetEncoders();
+  }
+
+  /** Zeroes the heading of the robot. */
+  public void zeroHeading() {
+    m_gyro.reset();
+  }
+
+  /**
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from -180 to 180
+   */
+  public double getHeading() {
+    return Rotation2d.fromDegrees(m_gyro.getAngle()%360).getDegrees();
+  }
+
+  public ChassisSpeeds getChassisSpeeds(){
+    return DriveConstants.kDriveKinematics.toChassisSpeeds(
+      m_frontLeft.getState(),
+      m_frontRight.getState(),
+      m_rearLeft.getState(),
+      m_rearRight.getState()
+    );
+  }
+
+  public void chassisDrive(ChassisSpeeds speeds){
+    double xSpeed = speeds.vxMetersPerSecond;
+    double ySpeed = speeds.vyMetersPerSecond;
+    double omega = speeds.omegaRadiansPerSecond;
+    drive(xSpeed, ySpeed, omega, false);
+  }
+
+  
+  
+
+  /**
+   * Returns the turn rate of the robot.
+   *
+   * @return The turn rate of the robot, in degrees per second
+   */
+  public double getTurnRate() {
+    return m_gyro.getRate() * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
+  }
+
+  /**
+   * Resets the gyroscope to 0 degrees
+   */
+  public void resetGyro(){
+    m_gyro.reset();
+  }
+
+  /**
+   * Gets angle of the gyro in degrees
+   */
+  public double getGyroAngle(){
+    return m_gyro.getAngle()%360;
+  }
+}
