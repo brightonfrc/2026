79fe62212bd3a64ac9c3c2a35cc260e1327e87e0
Vincent Jones <vincent.jones.vaj@gmail.com>
2026-02-25 18:16:26 +0000

    Fixed some deprecations

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -29,8 +29,8 @@ public final class Configs {
                     .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
                     // These are example gains you may need to them for your own robot!
                     .pid(0.04, 0, 0)
-                    .velocityFF(drivingVelocityFeedForward)
                     .outputRange(-1, 1);
+            drivingConfig.closedLoop.feedForward.kV(drivingVelocityFeedForward);
 
             turningConfig
                     .idleMode(IdleMode.kBrake)
